87dd19dd2ea53ca0df27dc650db6907c8c0d5cf9
Adalberto Cajueiro <adalberto.cajueiro@gmail.com>
2020-06-18 20:00:00 -0300

    Classe Util lidando com bases de dados com quantidades de colunas diferentes. Depende do nome das colunas (que precisa ser preservado).

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -19,7 +19,35 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Util {
-	public static Pattern PATTERN_DATA = Pattern.compile("[0-9]{2}/[0-9]{2}/[0-9]{2}");
+	public static final Pattern PATTERN_DATA = Pattern.compile("[0-9]{2}/[0-9]{2}/[0-9]{2}");
+	public static final String TOKEN_NUMERO_NOTIFICACAO = "Número da Notificação";
+	public static final String TOKEN_EH_PROFISSIONAL_SAUDE = "É profissional de saúde?";
+	public static final String TOKEN_TIPO_TESTE = "Tipo de Teste";
+	public static final String TOKEN_ESTADO_RESIDENCIA = "Estado de Residência";
+	public static final String TOKEN_DATA_NOTIFICACAO = "Data da Notificação";
+	public static final String TOKEN_FEBRE = "Febre";
+	public static final String TOKEN_TOSSE = "Tosse";
+	public static final String TOKEN_OUTROS = "Outros";
+	public static final String TOKEN_DOR_GARGANTA = "Dor de Garganta";
+	public static final String TOKEN_DISPNEIA = "Dispneia";
+	public static final String TOKEN_CEP = "CEP";
+	public static final String TOKEN_RESULTADO_TESTE = "Resultado do Teste";
+	public static final String TOKEN_SEXO = "Sexo";
+	public static final String TOKEN_ESTADO_TESTE = "Estado do Teste";
+	public static final String TOKEN_DOENCA_RESPIRATORIA_DESCOMPENSADA = "Doenças respiratórias crônicas descompensadas";
+	public static final String TOKEN_DOENCA_RENAL_AVANCADA = "Doenças renais crônicas em estágio avançado (graus 3, 4 ou 5)";
+	public static final String TOKEN_DOENCA_CROMOSSOMICA = "Portador de doenças cromossômicas ou estado de fragilidade imunológica";
+	public static final String TOKEN_DIABETES = "Diabetes";
+	public static final String TOKEN_IMUNOSSUPRESSAO = "Imunossupressão";
+	public static final String TOKEN_DOENCA_CARDIACA_CRONICA = "Doenças cardí­acas crônicas";
+	public static final String TOKEN_GESTANTE_ALTO_RISCO = "Gestante de alto risco";
+	public static final String TOKEN_BAIRRO = "Bairro";
+	public static final String TOKEN_DATA_COLETA_TESTE = "Data de coleta do teste";
+	public static final String TOKEN_DESCRICAO_SINTOMA = "Descrição do Sintoma";
+	public static final String TOKEN_DATA_ENCERRAMENTO = "Data de encerramento";
+	public static final String TOKEN_DATA_NASCIMENTO = "Data de Nascimento";
+	public static final String TOKEN_MUNICIPIO = "Municí­pio de Residência";
+	public static final String TOKEN_DATA_INICIO_SINTOMAS = "Data do iní­cio dos sintomas";
 	
 	public static String cleanString(String string) {
 		string = string.replaceAll("Ãº", "ú");
@@ -190,117 +218,261 @@ public class Util {
 			for (int i = 1; i <= lastRowNum; i++) {
 				 Row currentRow = sheet.getRow(i);
 				 Record record = new Record();
+				 int currentColumnIndex = 0;
+				 //buscar nas colunas carregadas se tem cada coluna pelo nome (TOKEN) e pega o index dela
 				 
-				 Cell currentCell = currentRow.getCell(0);
-				 String numeroNotificacao = getCellContentAsString(currentCell);
+				 ColumnIndexName column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_NUMERO_NOTIFICACAO)).findFirst().orElse(null);
+				 String numeroNotificacao = "";
+				 Cell currentCell = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //0
+					 numeroNotificacao = getCellContentAsString(currentCell);
+				 } 
 				 record.setNumeroNotificacao(numeroNotificacao);
 				 
-				 currentCell = currentRow.getCell(1);
-				 boolean profissionalAreaSaude = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_EH_PROFISSIONAL_SAUDE)).findFirst().orElse(null);
+				 boolean profissionalAreaSaude = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //1
+					 profissionalAreaSaude = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setProfissionalAreaSaude(profissionalAreaSaude);
 				 
-				 currentCell = currentRow.getCell(5);
-				 TipoTeste tipoTeste = getCellContentAsTipoTeste(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_TIPO_TESTE)).findFirst().orElse(null);
+				 TipoTeste tipoTeste = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //5
+					 tipoTeste = getCellContentAsTipoTeste(currentCell);
+				 }
 				 record.setTipoTeste(tipoTeste);
 				 
-				 currentCell = currentRow.getCell(6);
-				 String estadoResidencia = getCellContentAsString(currentCell);
+				 
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_ESTADO_RESIDENCIA)).findFirst().orElse(null);
+				 String estadoResidencia = "";
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //6
+					 estadoResidencia = getCellContentAsString(currentCell);
+				 }
 				 record.setEstadoResidencia(estadoResidencia);
 				 
-				 currentCell = currentRow.getCell(8);
-				 GregorianCalendar dataNotificacao = getCellContentAsData(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DATA_NOTIFICACAO)).findFirst().orElse(null);
+				 GregorianCalendar dataNotificacao = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //8
+					 dataNotificacao = getCellContentAsData(currentCell);
+				 }
 				 record.setDataNotificacao(dataNotificacao);
 				 
-				 currentCell = currentRow.getCell(11);
-				 boolean febre = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_FEBRE)).findFirst().orElse(null);
+				 boolean febre = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //11
+					 febre = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setFebre(febre);
 				 
-				 currentCell = currentRow.getCell(12);
-				 boolean tosse = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_TOSSE)).findFirst().orElse(null);
+				 boolean tosse = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //12
+					 tosse = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setTosse(tosse);
 				 
-				 currentCell = currentRow.getCell(13);
-				 boolean outros = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_OUTROS)).findFirst().orElse(null);
+				 boolean outros = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //13
+					 outros = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setOutros(outros);
 				 
-				 currentCell = currentRow.getCell(14);
-				 boolean dorGarganta = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DOR_GARGANTA)).findFirst().orElse(null);
+				 boolean dorGarganta = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //14
+					 dorGarganta = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDorGarganta(dorGarganta);
 				 
-				 currentCell = currentRow.getCell(15);
-				 boolean dispneia = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DISPNEIA)).findFirst().orElse(null);
+				 boolean dispneia = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //15
+					 dispneia = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDispneia(dispneia);
 				 
-				 currentCell = currentRow.getCell(16);
-				 String CEP = getCellContentAsString(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_CEP)).findFirst().orElse(null);
+				 String CEP = "";
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //16
+					 CEP = getCellContentAsString(currentCell);
+				 }
 				 record.setCEP(CEP);
 				 
-				 currentCell = currentRow.getCell(18);
-				 boolean resultadoTeste = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_RESULTADO_TESTE)).findFirst().orElse(null);
+				 boolean resultadoTeste = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //18
+					 resultadoTeste = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setResultadoTeste(resultadoTeste);
 				 
-				 currentCell = currentRow.getCell(20);
-				 Sexo sexo = getCellContentAsSexo(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_SEXO)).findFirst().orElse(null);
+				 Sexo sexo = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //20
+					 sexo = getCellContentAsSexo(currentCell);
+				 }
 				 record.setSexo(sexo);
 				 
-				 currentCell = currentRow.getCell(23);
-				 EstadoTeste estadoTeste = getCellContentAsEstadoTeste(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_ESTADO_TESTE)).findFirst().orElse(null);
+				 EstadoTeste estadoTeste = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //23
+					 estadoTeste = getCellContentAsEstadoTeste(currentCell);
+				 }
 				 record.setEstadoTeste(estadoTeste);
 				 
-				 currentCell = currentRow.getCell(24);
-				 boolean doencaRespiratoriaDescompensada = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DOENCA_RESPIRATORIA_DESCOMPENSADA)).findFirst().orElse(null);
+				 boolean doencaRespiratoriaDescompensada = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //24
+					 doencaRespiratoriaDescompensada = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDoencaRespiratoriaDescompensada(doencaRespiratoriaDescompensada);
 				 
-				 currentCell = currentRow.getCell(25);
-				 boolean doencaRenalAvancada = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DOENCA_RENAL_AVANCADA)).findFirst().orElse(null);
+				 boolean doencaRenalAvancada = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //25
+					 doencaRenalAvancada = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDoencaRenalAvancada(doencaRenalAvancada);
 				 
-				 currentCell = currentRow.getCell(26);
-				 boolean doencaCromossomicaImunologica = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DOENCA_CROMOSSOMICA)).findFirst().orElse(null);
+				 boolean doencaCromossomicaImunologica = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //26
+					 doencaCromossomicaImunologica = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDoencaCromossomicaImunologica(doencaCromossomicaImunologica);
 				 
-				 currentCell = currentRow.getCell(27);
-				 boolean diabetes = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DIABETES)).findFirst().orElse(null);
+				 boolean diabetes = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //27
+					 diabetes = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDiabetes(diabetes);
 				 
-				 currentCell = currentRow.getCell(28);
-				 boolean imunossupressao = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_IMUNOSSUPRESSAO)).findFirst().orElse(null);
+				 boolean imunossupressao = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //28
+					 imunossupressao = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setImunossupressao(imunossupressao);
 				 
-				 currentCell = currentRow.getCell(29);
-				 boolean doencaCardiacaCronica = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DOENCA_CARDIACA_CRONICA)).findFirst().orElse(null);
+				 boolean doencaCardiacaCronica = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //29
+					 doencaCardiacaCronica = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setDoencaCardiacaCronica(doencaCardiacaCronica);
 				 
-				 currentCell = currentRow.getCell(30);
-				 boolean gestanteAltoRisco = getCellContentAsBoolean(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_GESTANTE_ALTO_RISCO)).findFirst().orElse(null);
+				 boolean gestanteAltoRisco = false;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //30
+					 gestanteAltoRisco = getCellContentAsBoolean(currentCell);
+				 }
 				 record.setGestanteAltoRisco(gestanteAltoRisco);
 				 
-				 currentCell = currentRow.getCell(31);
-				 String bairro = getCellContentAsString(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_BAIRRO)).findFirst().orElse(null);
+				 String bairro = "";
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //31
+					 bairro = getCellContentAsString(currentCell);
+				 }
 				 record.setBairro(bairro);
 				 
-				 currentCell = currentRow.getCell(32);
-				 GregorianCalendar dataColetaTeste = getCellContentAsData(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DATA_COLETA_TESTE)).findFirst().orElse(null);
+				 GregorianCalendar dataColetaTeste = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //32
+					 dataColetaTeste = getCellContentAsData(currentCell);
+				 }
 				 record.setDataColetaTeste(dataColetaTeste);
 				 
-				 currentCell = currentRow.getCell(34);
-				 String descricaoSintoma = getCellContentAsString(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DESCRICAO_SINTOMA)).findFirst().orElse(null);
+				 String descricaoSintoma = "";
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //34
+					 descricaoSintoma = getCellContentAsString(currentCell);
+				 }
 				 record.setDescricaoSintoma(descricaoSintoma);
 				 
-				 currentCell = currentRow.getCell(35);
-				 GregorianCalendar dataEncerramento = getCellContentAsData(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DATA_ENCERRAMENTO)).findFirst().orElse(null);
+				 GregorianCalendar dataEncerramento = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //35
+					 dataEncerramento = getCellContentAsData(currentCell);
+				 }
 				 record.setDataEncerramento(dataEncerramento);
 				 
-				 currentCell = currentRow.getCell(36);
-				 GregorianCalendar dataNascimento = getCellContentAsData(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DATA_NASCIMENTO)).findFirst().orElse(null);
+				 GregorianCalendar dataNascimento = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //36
+					 dataNascimento = getCellContentAsData(currentCell);
+				 }
 				 record.setDataNascimento(dataNascimento);
 				 
-				 currentCell = currentRow.getCell(38);
-				 String municipio = getCellContentAsString(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_MUNICIPIO)).findFirst().orElse(null);
+				 String municipio = "";
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //38
+					 municipio = getCellContentAsString(currentCell);
+				 }
 				 record.setMunicipio(municipio);
 				 
-				 currentCell = currentRow.getCell(44);
-				 GregorianCalendar dataInicioSintomas = getCellContentAsData(currentCell);
+				 column = columns.stream().filter(elem -> elem.getName().equalsIgnoreCase(TOKEN_DATA_INICIO_SINTOMAS)).findFirst().orElse(null);
+				 GregorianCalendar dataInicioSintomas = null;
+				 if(column != null) {
+					 currentColumnIndex = column.getIndex();
+					 currentCell = currentRow.getCell(currentColumnIndex); //44
+					 dataInicioSintomas = getCellContentAsData(currentCell);
+				 }
 				 record.setDataInicioSintomas(dataInicioSintomas);
 				 
 				 records.add(record);
@@ -315,7 +487,7 @@ public class Util {
 				wb.close();
 			}
 		}
-        
+        System.out.println("Number of records: " + dataInfo.getNumberOfRecords());
 		
 		
 		return dataInfo;
@@ -331,13 +503,15 @@ public class Util {
 		//os arquivos devem ser subidos para uma pasta especifica via servico 
 		//pela data do arquivo mais recente (ou pelo nome) creio qu eseja possivel saber 
 		//qual arquivo seria o mais recente.
-		return new File("/Users/adalbertocajueiro/Downloads/Base 03-06 UFCG.xlsx");
+		return new File("/Users/adalbertocajueiro/Downloads/Base 16_06 UFCG.xlsx");
 	}
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
-		File file = new File("/Users/adalbertocajueiro/Downloads/Base 03-06 UFCG.xlsx");
+		File file = new File("/Users/adalbertocajueiro/Downloads/Base 16_06 UFCG.xlsx");
 		//System.out.println("Sim".equalsIgnoreCase("sim"));
 		//System.out.println(cleanString("NÃ£o").equalsIgnoreCase("não"));
 		DataInfo dados = loadDataFromExcel(file);
-		System.out.println(dados.getNumberOfRecords());
+		//dados.getLoadedColumns().forEach(d -> System.out.println(d.getName()));
+		long filtrados = dados.getRecords().stream().filter(r -> r.getEstadoResidencia().equalsIgnoreCase("Paraí­ba")).count();
+		System.out.println("Registros filtrados: " + filtrados);
 	}
 }
